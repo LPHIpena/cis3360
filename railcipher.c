d4889ce045c5f705b10a1d27ad309a5b93b87116
Louis Pena <penalp9805@gmail.com>
2014-10-17 21:48:56 -0400

    files

--- railcipher.c
+++ railcipher.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <assert.h>
 int main(int argc, char** argv){
+    assert(argc == 3);
+    
+    FILE* keyFile = fopen(argv[1], "r");
+    FILE* output = fopen(argv[2], "w");
 
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
     return 0;
 }
